2a43177f922117632b37a566f11a90452919604a
sdwsgkj <37923209+sdwsgkj@users.noreply.github.com>
2019-04-01 20:33:33 +0900

    테스트

--- src/main/java/com/crud/ideacrm/controller/CustController.java
+++ src/main/java/com/crud/ideacrm/controller/CustController.java
@@ -49,7 +49,7 @@ public class CustController {
         return mView;
     }
 
-    @RequestMapping(value = "/abbbb", method = RequestMethod.GET)
+    @RequestMapping(value = "/abbbbㅋㅋㅋㅋㅋㅋ", method = RequestMethod.GET)
     @ResponseBody
     public List<Map<String, Object>> test(HttpServletRequest request){
         ModelAndView mav = new ModelAndView();

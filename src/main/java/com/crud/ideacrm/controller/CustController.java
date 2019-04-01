6a9766c63040938173d6bc69f996fbf1ace41df6
agdwlsduf <43662923+agdwlsduf@users.noreply.github.com>
2019-04-01 20:38:27 +0900

    ㅅㄷㄴㅅ

--- src/main/java/com/crud/ideacrm/controller/CustController.java
+++ src/main/java/com/crud/ideacrm/controller/CustController.java
@@ -49,7 +49,7 @@ public class CustController {
         return mView;
     }
 
-    @RequestMapping(value = "/w3werwerwer", method = RequestMethod.GET)
+    @RequestMapping(value = "/ㅁㄴㅇㄻㄴㅇㄻㄴ", method = RequestMethod.GET)
     @ResponseBody
     public List<Map<String, Object>> test(HttpServletRequest request){
         ModelAndView mav = new ModelAndView();

fd958a5001aafd157fdca0ff72f1b5c54c93f3ec
agdwlsduf <43662923+agdwlsduf@users.noreply.github.com>
2019-04-04 16:40:13 +0900

    merge test

--- src/main/java/com/crud/ideacrm/controller/CustController.java
+++ src/main/java/com/crud/ideacrm/controller/CustController.java
@@ -23,7 +23,7 @@ public class CustController {
     CustService custService;
     private final int USINGMENU = 1;//고객의 사용 메뉴 값은 1
 
-    //고객 리스트 기본 화면.
+    //고객 리스트 기본 화면.test
     @RequestMapping(value = "/cust", method = RequestMethod.GET)
     public ModelAndView authCustList(HttpServletRequest request){
         int siteId = Integer.parseInt(request.getSession().getAttribute("SITEID").toString());

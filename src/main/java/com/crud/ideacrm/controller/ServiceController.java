7ce4e8ad58c885e796732cd464f9f28f6ed25431
agdwlsduf <43662923+agdwlsduf@users.noreply.github.com>
2019-04-04 17:01:20 +0900

    merge test12358

--- src/main/java/com/crud/ideacrm/controller/ServiceController.java
+++ src/main/java/com/crud/ideacrm/controller/ServiceController.java
@@ -98,7 +98,7 @@ public class ServiceController {
     }
 
     @RequestMapping(value = "/service/calendar", method = RequestMethod.GET)
-    public ModelAndView serviceCalendar(HttpServletRequest request){
+    public ModelAndView serviceCalendar(HttpServletRequest request){//123
         ModelAndView mView = new ModelAndView();
         mView.setViewName("page/service/calendar/serviceCalendar");
         return mView;
